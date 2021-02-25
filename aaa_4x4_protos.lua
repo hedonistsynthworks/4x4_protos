fa74f6303166b414cfc2635ea5127821d5b79fea
Daniel Hengeveld <danielwh@gmail.com>
2021-02-25 04:15:40 -0600

    add modal note sequencer

--- aaa_4x4_protos.lua
+++ aaa_4x4_protos.lua
@@ -58,7 +58,8 @@ local state = {
   note_entry = false,
   current_channel = 1,
   position = 1,
-  note_mode = false
+  note_mode = 0,
+  note_blink = false
 }
 
 function configure_sampler()
@@ -69,6 +70,35 @@ function configure_sampler()
     engine.playMode(i, 3)
     engine.ampAttack(i, 0)
   end
+end
+
+function step_to_coords(step)
+  local dec = step - 1
+  local _x = (dec % 4) + 1
+  local _y = (math.floor((dec - x) / 4)) + 6
+  local coords = {
+    x = _x,
+    y = _y
+  }
+  return coords
+end
+
+-- This is called for every step in a loop
+function light_note_track_step(step, on_brightness)
+  brightness = 0
+  -- mode blink is handled in light_steps()
+  if state.note_mode == 0
+  then
+    if step == state.position
+    then
+      brightness = 8
+    elseif state.channels[4][step] > 0
+    then
+      brightness = on_brightness
+    end
+  end
+  coords = step_to_coords(step)
+  g:led(coords.x, coords.y, brightness)
 end
 
 function light_step(step, on_brightness)
@@ -82,6 +112,9 @@ function light_step(step, on_brightness)
   elseif state.current_channel <= 3 and state.channels[state.current_channel][step]
   then
     brightness = on_brightness
+  elseif state.current_channel == 4 and state.channels[state.current_channel][step] > 0
+  then
+    light_note_track_step(step, on_brightness)
   end
   g:led(x + 1, y + 5, brightness)
 end
@@ -89,6 +122,11 @@ end
 function light_steps()
   for i = 1,16 do
     light_step(i, 15)
+  end
+  if state.note_blink == true and state.note_mode > 0 and state.current_channel == 4
+  then
+    coords = step_to_coords(state.note_mode)
+    g:led(coords.x, coords.y, 15)
   end
 end
 
@@ -103,9 +141,21 @@ function on_channel_key(x)
   state.current_channel = x
 end
 
+function on_note_key(step)
+  if state.note_mode > 0
+  then
+    state.channels[4][state.note_mode] = step
+    state.note_mode = 0
+  else
+    state.note_mode = step
+  end
+end
+
 function on_step_key(step)
   if state.current_channel <= 3 then
     state.channels[state.current_channel][step] = not state.channels[state.current_channel][step]
+  else
+    on_note_key(step)
   end
 end
 
@@ -145,6 +195,7 @@ end
 
 function step()
   state.position = (state.position % 16) + 1
+  state.note_blink = not state.note_blink
   draw()
   play(state.position)
 end
